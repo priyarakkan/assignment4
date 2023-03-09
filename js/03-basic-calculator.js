c77af62f9e566aef55bfcb24a45469f48510b66d
priyarakkan <priyadharsinikdp@gmail.com>
2023-03-08 17:22:15 -0800

    Update 03-basic-calculator.js

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,8 +1,8 @@
 // IMPORT THE MODULE
-import { add } from './modules/calculator.js';
-import { sub } from './modules/calculator.js';
-import { mul } from './modules/calculator.js';
-import { div } from './modules/calculator.js';
+import {add} from './modules/calculator.js';
+import {sub} from './modules/calculator.js';
+import {mul} from './modules/calculator.js';
+import {div} from './modules/calculator.js';
 
 
 let num1 = parseInt(prompt('Enter the first number : '));
