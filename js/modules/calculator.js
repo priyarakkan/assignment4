56f899d18c157d8adb6debf5c3bea3054ac0677e
priyarakkan <priyadharsinikdp@gmail.com>
2023-03-16 13:39:57 -0700

    Update calculator.js

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,30 +1,51 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+const calculate = (num1, num2, method) => {
+    switch (method) {
+     case 'add':
+         return num1 + num2;
+         break;
+     case 'subtract':
+         return num1 - num2;
+         break;
+     case 'multiply':
+         return num1 * num2;
+         break;
+     case 'div':
+         return num1 / num2;
+         break;
+     default:
+         return 0;
+    }
+ };
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
 const add = (num1, num2) => {
-    return num1 + num2;
+    total = calculate(num1, num2, 'add');
+    return total;
 };
 
 // SUBTRACT FUNCTION
-const sub = (num1, num2) => {
-    return num1 - num2;
-  
+const subtract = (num1, num2) => {
+    total = calculate(num1, num2, 'subtract');
+    return total;
 };
 
 // MULTIPLY FUNCTION
-const mul = (num1, num2) => {
-    return num1 * num2;
+const multiply = (num1, num2) => {
+    total = calculate(num1, num2, 'multiply');
+    return total;
 };
 
 // DIVIDE FUNCTION
 const div = (num1, num2) => {
-    return num1 / num2;
+    total = calculate(num1, num2, 'div');
+    return total;
 };
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
 export {add};
-export {sub};
-export {mul};
+export {subtract};
+export {multiply};
 export {div}; 
