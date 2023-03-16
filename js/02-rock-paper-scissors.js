f361b7a3158c98baf267df535e5e163201484639
priyarakkan <priyadharsinikdp@gmail.com>
2023-03-16 13:30:25 -0700

    Update 02-rock-paper-scissors.js

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -3,27 +3,35 @@ let again;
 
 do {
     let userChoice = prompt('Rock(0), paper(1) or scissor(2) choice any one:');
-    let computerChoice = Math.round(Math.random() * 3);
+    let computerNumber = Math.round(Math.random() * 3);
+if(computerNumber == 0){
+    computerChoice = 'rock';
+} else if(computerNumber == 1){
+    computerChoice = 'paper';
+} else if (computerNumber == 2){
+    computerChoice = 'scissors'
+}
+
     console.log(`${userChoice}`);
     console.log(`${computerChoice}`);
-    if( (userChoice=='rock'&& computerChoice==2)
-        ||(userChoice=='paper'&& computerChoice==0)
-        ||(userChoice=='scissor'&& computerChoice==1) ) 
+    if( (userChoice=='rock'&& computerChoice== 'scissors')
+        ||(userChoice=='paper'&& computerChoice== 'rock')
+        ||(userChoice=='scissors'&& computerChoice== 'paper') ) 
     {
     alert(`You are the winner!`);
-    } else if( (userChoice=='rock'&& computerChoice==1)
-         ||(userChoice=='paper'&& computerChoice==2)
-         ||(userChoice=='scissor'&& computerChoice==0) ) 
+    } else if( (userChoice=='rock'&& computerChoice== 'paper')
+         ||(userChoice=='paper'&& computerChoice== 'scissors')
+         ||(userChoice=='scissors'&& computerChoice== 'rock') ) 
     {
     alert(`Sorry you lose the game. Computer win the game!`);
-    } else if( (userChoice=='rock'&& computerChoice==0)
-           ||(userChoice=='paper'&& computerChoice==1)
-           ||(userChoice=='scissor'&& computerChoice==2) )
+    } else if( (userChoice=='rock'&& computerChoice== 'rock')
+           ||(userChoice=='paper'&& computerChoice== 'paper')
+           ||(userChoice=='scissosr'&& computerChoice== 'scissors') )
     {
     alert(`Match is tie!`);
-    }  else if ((userChoice != 'rock') || (userChoice != 'paper') || (userChoice != 'scissor'))
+    }  else if ((userChoice != 'rock') || (userChoice != 'paper') || (userChoice != 'scissors'))
     {
-        alert(`Enter correct option among rock, paper or scirror`);
+        alert(`Enter correct option among rock, paper or scirrors`);
     } 
     again = prompt('Play again? (y/n)')
 } while(again === 'y');
